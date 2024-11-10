bc9715fcf410cf85b2282fd6dea79e1837be5fcf
souleymanediallo <souleymanedialloo@yahoo.fr>
2024-11-10 16:16:49 +0100

    custom home page

--- cities/templatetags/city_tags.py
+++ cities/templatetags/city_tags.py
@@ -6,5 +6,5 @@ register = template.Library()
 
 @register.inclusion_tag('cities/city_list.html')
 def show_all_cities():
-    cities = City.objects.all()
+    cities = City.objects.all().order_by('name')
     return {'cities': cities}
